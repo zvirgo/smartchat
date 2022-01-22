fe21148de0ff0176f27f7b0b8b559a9c40519773
zeinali <zeinali.b69@yahoo.com>
2022-01-22 14:22:51 +0330

    configure firebase setup

---
+++ src/boot/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl04YnymOpyBJ-pnjzfGWpCR44yaxJDg4",
+  authDomain: "smartchat-6ce8b.firebaseapp.com",
+  projectId: "smartchat-6ce8b",
+  storageBucket: "smartchat-6ce8b.appspot.com",
+  messagingSenderId: "144342713608",
+  appId: "1:144342713608:web:5696d5d6e8576ce53118bd"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp.firestore();
---
+++ src/store/storeX.js
@@ -0,0 +1,12 @@
+const state = {};
+const mutation = {};
+const actions = {};
+const getters = {};
+
+export default {
+  namespaced: true,
+  state,
+  mutation,
+  actions,
+  getters,
+};
