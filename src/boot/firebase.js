6ed0ea719a0f67ba51d21bed40261af5d56df54e
zeinali <zeinali.b69@yahoo.com>
2022-01-22 14:41:21 +0330

    fix authorization firebase

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBl04YnymOpyBJ-pnjzfGWpCR44yaxJDg4",
@@ -8,8 +8,10 @@ const firebaseConfig = {
   projectId: "smartchat-6ce8b",
   storageBucket: "smartchat-6ce8b.appspot.com",
   messagingSenderId: "144342713608",
-  appId: "1:144342713608:web:5696d5d6e8576ce53118bd"
+  appId: "1:144342713608:web:5696d5d6e8576ce53118bd",
 };
 // Initialize Firebase
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-export default firebaseApp.firestore();
+const firebaseAuth = firebaseApp.auth();
+export { firebaseAuth, firebaseApp };
